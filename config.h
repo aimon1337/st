66d823c4d2eb2b43494bc7afec81f7c4504f5dde
Andrei <andrei.corbu@carabella.ro>
2020-04-12 00:14:55 +0300

    Updated fonts

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono:styke=Bold:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Fira Mono:style=Bold:pixelsize=12:antialias=true:autohint=true";
 //static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 //static char *font = "DejaVu Sans Mono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
