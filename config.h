3558dfe87be164214f1e1feee48c92b44cc192e8
Andrei Corbu <corbu.andrei@yahoo.com>
2020-05-26 17:37:48 +0300

    updated st

--- config.h
+++ config.h
@@ -5,11 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
+//static char *font = "xos4 Terminus:pixelsize=12:antialias=true:autohint=true";
 static char *font = "Fira Mono:style=Bold:pixelsize=12:antialias=true:autohint=true";
 //static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 //static char *font = "DejaVu Sans Mono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
-static int borderpx = 30;
+static int borderpx = 20;
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
